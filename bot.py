f91d49e977da858625f78c733eb0987f2b2fa9d8
UberHacker1 <uberhacker@list.ru>
2025-02-05 01:30:47 +0300

    version 1.2

--- bot.py
+++ bot.py
@@ -1,21 +1,60 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, CallbackContext
+import json
+
+# Пример данных заданий
+tasks = [
+    {
+        "question": "Укажите варианты ответов, в которых верно выделена буква, обозначающая ударный гласный звук. Запишите номера ответов.",
+        "options": [
+            "1)  созЫв",
+            "2)  Отзыв (посла из страны)",
+            "3)  дОнизу",
+            "4)  оптОвый",
+            "5)  аэропортЫ"
+        ],
+        "correctAnswers": [2, 5]
+    },
+    {
+        "question": "Укажите варианты ответов, в которых верно выделена буква, обозначающая ударный гласный звук. Запишите номера ответов.",
+        "options": [
+            "1)  звонИт",
+            "2)  красИвее",
+            "3)  кУхонный",
+            "4)  тОрты",
+            "5)  средствА"
+        ],
+        "correctAnswers": [1, 2, 5]
+    }
+]
 
 # Команда для запуска мини-приложения
-def start(update: Update, context: CallbackContext):
+async def start(update: Update, context: CallbackContext):
+    # Преобразуем данные заданий в JSON
+    tasks_json = json.dumps(tasks)
+    
+    # Создаем кнопку для открытия мини-приложения
     keyboard = [
-        [InlineKeyboardButton("Открыть мини-приложение", web_app={"url": "https://ваш-хостинг.ру"})]
+        [InlineKeyboardButton("Открыть мини-приложение", web_app={"url": "https://ege4tgapp.vercel.app"})]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Нажмите кнопку, чтобы начать:", reply_markup=reply_markup)
+    
+    # Отправляем сообщение с кнопкой
+    await update.message.reply_text(
+        "Нажмите кнопку, чтобы начать:",
+        reply_markup=reply_markup
+    )
 
 # Основная функция
 def main():
-    updater = Updater("7202820544:AAEd0KJbxzsOpH_sAUEXVywWU0Tho7KxsaQ")
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+    # Вставьте сюда ваш токен
+    application = Application.builder().token("7202820544:AAEd0KJbxzsOpH_sAUEXVywWU0Tho7KxsaQ").build()
+
+    # Регистрируем обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
